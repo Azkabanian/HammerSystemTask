3c0c5ce543f0840efa5e90e6b573894468fc0bbf
ArturYusupov <YusupovArturrr@gmail.com>
2022-10-13 20:05:12 +0300

    added bottom navigation

--- app/src/main/java/android/hammersystemtest/MainActivity.kt
+++ app/src/main/java/android/hammersystemtest/MainActivity.kt
@@ -1,11 +1,23 @@
 package android.hammersystemtest
 
+import android.hammersystemtest.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/android/hammersystemtest/feature/cartFragment/CartFragment.kt
+++
@@ -1,8 +0,0 @@
-package android.hammersystemtest.feature.cartFragment
-
-import android.hammersystemtest.databinding.FragmentCartBinding
-import android.hammersystemtest.feature.baseFragment.BaseFragment
-
-class CartFragment : BaseFragment<FragmentCartBinding>(FragmentCartBinding::inflate) {
-
-}
