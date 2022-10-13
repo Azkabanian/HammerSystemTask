ff8e64b3986afa029eb2b1192d5524754035feac
ArturYusupov <YusupovArturrr@gmail.com>
2022-10-13 17:12:15 +0300

    created BaseFragment.kt, ProfileFragment.kt, CartFragment.kt

---
+++ app/src/main/java/android/hammersystemtest/feature/cartFragment/CartFragment.kt
@@ -0,0 +1,8 @@
+package android.hammersystemtest.feature.cartFragment
+
+import android.hammersystemtest.databinding.FragmentCartBinding
+import android.hammersystemtest.feature.baseFragment.BaseFragment
+
+class CartFragment : BaseFragment<FragmentCartBinding>(FragmentCartBinding::inflate) {
+
+}
